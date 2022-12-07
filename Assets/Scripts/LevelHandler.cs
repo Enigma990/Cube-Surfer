725f6e944f3fbdc7921be225b3952792f71d8f0d
Davinder Singh <dvndrsingh786@gmail.com>
2022-12-07 16:01:06 +0530

    Working on platformer

--- Assets/Scripts/LevelHandler.cs
+++ Assets/Scripts/LevelHandler.cs
@@ -6,19 +6,26 @@ public class LevelHandler : MonoBehaviour
 {
     public static LevelHandler instance;
 
-    public GameObject blockerPrefab;
+    [Header("Floor Data")]
+    public int floorsCount;
+    public GameObject floorPrefab;
+    public Transform floorsParent;
+    public float floorSpacing;
+
+    [Header("Wall Data")]
+    public GameObject wallPrefab;
     [Range(1, 10)]
-    public int blockerHeightMax = 1;
+    public int wallHeightMax = 1;
     [Range(1, 8)]
-    public int blockerWidthMax = 1;
-    public GameObject foodPrefab;
+    public int wallWidthMax = 1;
+
+    [Header("Collectable Data")]
     public GameObject collectablePrefab;
     [Range(1, 15)]
     public int collectableHeightMax = 1;
-    public GameObject floorPrefab;
-    public Transform floorsParent;
-    public int levelLength;
-    public float floorLength;
+
+    [Header("Food Data")]
+    public GameObject foodPrefab;
 
 
     private void Awake()
@@ -37,28 +44,76 @@ public class LevelHandler : MonoBehaviour
 
     private void Start()
     {
-        CreateNewLevel();
+        //CreateNewLevel();
     }
-
+    [ContextMenu("Create Level")]
     void CreateNewLevel()
     {
-        for (int i = 0; i < levelLength; i++)
+        List<GameObject> kids = new List<GameObject>();
+        for (int i = 0; i < floorsParent.childCount; i++)
+        {
+            kids.Add(floorsParent.GetChild(i).gameObject);
+        }
+        foreach (var item in kids)
+        {
+            DestroyImmediate(item);
+        }
+        if (floorsParent.childCount > 0) return;
+        for (int i = 0; i < floorsCount; i++)
         {
             GameObject floor = Instantiate(floorPrefab, floorsParent);
-            floor.transform.localPosition = new Vector3(0, -4, i * floorLength);
+            floor.transform.localPosition = new Vector3(0, -4, i * floorSpacing);
             if (i > 0)
             {
-                
+                MakeWall(floor.transform, Random.Range(1, wallHeightMax + 1), Random.Range(1, wallWidthMax + 1));
+                //MakeWall(floor.transform, 1, 3);
             }
         }
     }
 
-    void MakeWall(int wallHeight, int wallWidth)
+    void MakeWall(Transform _floor, int _wallHeight, int _wallWidth)
     {
-        if (wallWidth % 2 == 0)
+        if (_wallWidth <= 0) return;
+        float xDiff = 0.125f;
+        float zPos = Random.Range(-0.45f, 0.45f);
+        if (_wallWidth % 2 == 0)
         {
-
+            for (int j = 0; j < _wallHeight; j++)
+            {
+                for (int i = 0; i < _wallWidth / 2; i++)
+                {
+                    GameObject wallObj = Instantiate(wallPrefab);
+                    wallObj.transform.SetParent(_floor);
+                    wallObj.transform.localPosition = new Vector3(0.06f + (xDiff * i), j + 1, zPos);
+                }
+                for (int i = 0; i < _wallWidth / 2; i++)
+                {
+                    GameObject wallObj = Instantiate(wallPrefab);
+                    wallObj.transform.SetParent(_floor);
+                    wallObj.transform.localPosition = new Vector3(-0.06f + (-xDiff * i), j + 1, zPos);
+                }
+            }
+        }
+        else
+        {
+            for (int j = 0; j < _wallHeight; j++)
+            {
+                GameObject wallObj01 = Instantiate(wallPrefab);
+                wallObj01.transform.SetParent(_floor);
+                wallObj01.transform.localPosition = new Vector3(0, j + 1, zPos);
+                for (int i = 0; i < (_wallWidth - 1) / 2; i++)
+                {
+                    GameObject wallObj = Instantiate(wallPrefab);
+                    wallObj.transform.SetParent(_floor);
+                    wallObj.transform.localPosition = new Vector3(xDiff + (xDiff * i), j + 1, zPos);
+                }
+                for (int i = 0; i < (_wallWidth - 1) / 2; i++)
+                {
+                    GameObject wallObj = Instantiate(wallPrefab);
+                    wallObj.transform.SetParent(_floor);
+                    wallObj.transform.localPosition = new Vector3(-xDiff + (-xDiff * i), j + 1, zPos);
+                }
+            }
         }
     }
-
 }
